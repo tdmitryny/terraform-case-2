3f4121b1290afd495c0fd738143156894040ce3c
Dmitry <tdmitryny@gmail.com>
2023-12-08 10:53:10 -0500

    Done with IaC

--- main.tf
+++ main.tf
@@ -34,3 +34,40 @@ resource "azurerm_virtual_network" "vnet" {
 
 
 }
+
+
+resource "azurerm_subnet" "sub-net" {
+  name                 = "sub-net-911"
+  resource_group_name  = azurerm_resource_group.mtc-rg-911.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.123.1.0/24"]
+
+}
+
+
+resource "azurerm_network_security_group" "nsg-911" {
+  name                = "nsg-911"
+  location            = azurerm_resource_group.mtc-rg-911.location
+  resource_group_name = azurerm_resource_group.mtc-rg-911.name
+
+  tags = {
+    enviroment = "dev"
+  }
+
+}
+
+
+resource "azurerm_network_security_rule" "nsg-rule" {
+  name                        = "nsg-rule-911"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc-rg-911.name
+  network_security_group_name = azurerm_network_security_group.nsg-911.name
+
+}
